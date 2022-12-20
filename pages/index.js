64c550386cd1a06a3e3976af71ce69691e580055
goodnihgtowl <junk@kawataki.org>
2022-12-19 22:36:59 -0800

    h

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from "react-dom";
+import DragDrop from '../components/drag-drop';
+
+export default function index() {
+  return (
+    <div>
+      <DragDrop/>
+    </div>
+  )
+}
